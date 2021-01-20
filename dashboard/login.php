93c5a44cb5822c36bb7621e47fb2caba6e235ae3
Gregory <r0613527@student.thomasmore.be>
2021-01-20 21:11:19 +0100

    setup update
    more files yeeeeeeeee!!!

--- dashboard/login.php
+++ dashboard/login.php
@@ -1,3 +1,25 @@
 <?php 
 
-?>
+?><!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="css/style.css">
+    <title>Login</title>
+</head>
+<body>
+    <div class="glass">
+        <img src="../images/logo.png" alt="logo">
+        <form action="" method="post">
+            <div class="form-group">
+                <span class="icon_user"></span><input type="email" placeholder="Enter email" name="email" id="email" class="form-control">
+            </div>
+            <div class="form-group">
+                <span class="icon_password"></span><input type="password" placeholder="Password" name="password" id="password" class="form-control">
+            </div>
+            <input type="submit" value="Login">
+        </form>
+    </div>
+</body>
+</html>
